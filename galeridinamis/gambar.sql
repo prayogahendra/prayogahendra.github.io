79791679c2931c5e79af36d5e9de15d3f00c8323
prayogahendra <yogayogahendra@gmail.com>
2018-07-06 19:11:43 +0700

    Add files via upload

---
+++ galeridinamis/gambar.sql
@@ -0,0 +1,9 @@
+INSERT INTO `gambar` (`id`, `nama_file`, `judul`, `diupload`,`status`) VALUES
+    (1, 'codingan-1.jpeg', 'Man Wearing Red White Keffiyeh', '2017-02-20 00:00:00','1'),
+    (2, 'codingan-2.jpg', 'Kid Praying', '2017-02-20 00:00:00','1'),
+    (3, 'codingan-3.jpeg', 'Church', '2017-02-20 00:00:00','1'),
+    (4, 'codingan-4.jpg', 'Monks Sitting on Black Floor', '2017-02-20 00:00:00','1'),
+    (5, 'codingan-5.jpeg', 'People Looking Mosque', '2017-02-20 00:00:00','1'),
+    (6, 'codingan-6.jpeg', 'Praying', '2017-02-20 00:00:00','1'),
+    (7, 'codingan-7.jpeg', 'Buddhism', '2017-02-20 00:00:00','1'),
+    (8, 'codingan-8.jpeg', 'Church', '2017-02-20 00:00:00','1');
