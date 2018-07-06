a382c98c5a89072978b31bf48317a340061a83cb
prayogahendra <yogayogahendra@gmail.com>
2018-07-06 19:13:20 +0700

    Add files via upload

---
+++ lombaotn/false_tebak1.php
@@ -0,0 +1,175 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Lomba OTN </title>
+</head>
+<meta charset="UTF-8">
+<audio src="backsound.mp3" autoplay="autoplay"></audio>
+	<meta charset="UTF-8" />
+		<title>Hitung</title>
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Acme" rel="stylesheet">
+		<!--[if IE]>
+  		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	<title>Aplikasi Kita</title>
+	<style>
+	body{
+		background-color: #f1c40f;
+		-webkit-animation: color 10s ease-in  0s infinite alternate running;
+		-moz-animation: color 10s linear  0s infinite alternate running;
+		animation: color 10s linear  0s infinite alternate running;
+		font-family: 'Acme', sans-serif;
+		font-color: #000000;
+	}
+
+	/* Animasi + Prefix untuk browser */
+	@-webkit-keyframes color {
+	    0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	@-moz-keyframes color {
+	     0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	@keyframes color {
+	  0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	h1{
+	   margin: 0;
+	   color: #190f76;
+	   text-transform: uppercase;
+	   font-size: 30pt;
+	   text-align: center;
+	   letter-spacing: 0.2em
+
+
+	}
+
+	h2{
+	   margin: 0;
+	   color: #2d1fb1;
+	   text-transform: uppercase;
+	   font-size: 15pt;
+	   text-align: center;
+	   letter-spacing: 0.2em
+	}
+	p{
+		text-align: center;
+	}
+	a{
+		text-decoration: none;
+		color: #333;
+	}
+	a:hover{
+  text-decoration: underline;
+}
+		 
+		 
+		.zoom-effect {  
+		  position: relative;
+		  width: 150px;
+		  height: 120px;
+		}
+		 
+		.kotak {
+		  position: absolute;
+		  top: 0;
+		  left: 30px;
+		  
+		}
+		 
+		.kotak img {
+		  -webkit-transition: 0.4s ease;
+		  transition: 0.4s ease;
+		  width: 150px;
+		}
+		 
+		.zoom-effect:hover .kotak img {
+		  -webkit-transform: scale(1.08);
+		  transform: scale(1.08);
+		}
+
+		.setzoom {
+			margin-top: 10px;
+			margin-right: 30px;	
+		}
+	</style>
+
+        </script>
+</head>
+<body>
+<table border="0" width="850px" align="center" height="150px" style="margin-top: 70px " background="9.jpg">
+<tr height="140px">
+ 	<td width="253px" colspan="4" align="center">
+ 	 <h2> jawaban kamu salah dek </h2>
+</tr>
+<tr height="140px" align="center">
+	<td width="220px" >
+		
+	</td>
+	<td rowspan="2">
+		   <div class="zoom-effect" align="center" >
+		<div class="kotak">
+			<a href="#"> <img src="false.png" width="220px" height="170px" align="center" class="setzoom" > </a>
+		</div>
+	</td>
+	<td rowspan="2">
+		  <div class="zoom-effect" align="center">
+		<div
+		 class="kotak">
+			<a href="tebak1.php"> <img src="rewind.png" width="220px" height="170px" align="center" class="setzoom" > </a>
+		</div>
+	</td>
+	<td>
+		 
+	</td>
+</tr>
+<tr height="130px">
+	<td width="220px">
+	  
+	</td>
+
+	<td>
+		
+	</td>
+	
+</tr>
+<tr height="70px">
+	<td width="220px">
+		
+	</td>
+
+	<td>
+		
+	</td>
+
+	<td>
+		
+	</td>
+
+	<td>
+	<div class="zoom-effect">
+		<div class="kotak">
+		<a href="index.php"><img src="home.png" width="40px" height="90px" class="setzoom"></a>
+	</div>
+	</div>
+	</td>	
+	
+</tr>
+
+
+
+</body>
+</html>
---
+++ lombaotn/index.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title> Lomba OTN </title>
+</head>
+<meta charset="UTF-8">
+	<meta charset="UTF-8" />
+		<title>Hitung</title>
+		<link rel="shortcut icon" href="../favicon.ico">
+		<link href="https://fonts.googleapis.com/css?family=Cabin+Sketch" rel="stylesheet">
+		<!--[if IE]>
+  		<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	<title>Aplikasi Kita</title>
+	<style>
+	body{
+		background-color: #f1c40f;
+		-webkit-animation: color 10s ease-in  0s infinite alternate running;
+		-moz-animation: color 10s linear  0s infinite alternate running;
+		animation: color 10s linear  0s infinite alternate running;
+		font-family: 'Cabin Sketch', cursive;
+		font-color: #000000;
+	}
+
+	/* Animasi + Prefix untuk browser */
+	@-webkit-keyframes color {
+	    0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	@-moz-keyframes color {
+	     0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	@keyframes color {
+	  0% { background-color: #f1c40f; }
+	    32% { background-color: #e74c3c; }
+	    55% { background-color: #9b59b6; }
+	    76% { background-color: #16a085; }
+	    100% { background-color: #2ecc71; }
+	}
+	h1{
+	   margin: 0;
+	   color: #4797e1;
+	   text-transform: uppercase;
+	   font-size: 30pt;
+	   text-align: center;
+	   letter-spacing: 0.2em
+
+
+	}
+
+	h2{
+	   margin: 0;
+	   color: #2d1fb1;
+	   text-transform: uppercase;
+	   font-size: 15pt;
+	   text-align: center;
+	   letter-spacing: 0.2em
+	}
+	p{
+		text-align: center;
+	}
+	a{
+		text-decoration: none;
+		color: #333;
+	}
+	a:hover{
+  text-decoration: underline;
+}
+		 
+		 
+		.zoom-effect {  
+		  position: relative;
+		  width: 180px;
+		  height: 180px;
+		  margin: 0 auto;
+		  overflow: hidden; 
+		  margin-right: 70px; 
+		  margin-top: 20px;
+		}
+		 
+		.kotak {
+		  position: absolute;
+		  top: 0;
+		  left: 30px;
+		  
+		}
+		 
+		.kotak img {
+		  -webkit-transition: 0.4s ease;
+		  transition: 0.4s ease;
+		  width: 150px;
+		}
+		 
+		.zoom-effect:hover .kotak img {
+		  -webkit-transform: scale(1.08);
+		  transform: scale(1.08);
+		}
+	</style>
+
+        </script>
+</head>
+<body>
+<audio src="backsound.mp3" autoplay="autoplay"></audio>
+<table border="0" width="850px" align="center" height="150px" style="margin-top: 70px " background="bgclev2.png">
+<tr height="165px">
+ 	<td width="253px">
+
+
+ 	</td>	
+	<td
+
+ 	</td>
+ 	<td>
+ 		
+
+ 	</td>
+</tr>
+<tr height="190px">
+	<td width="200px">
+		
+	</td>
+	<td width="250px">
+	<div class="zoom-effect">
+		<div class="kotak">
+			<a href="hitung1.php"> <img src="next.png" width="150px" height="150px" > </a>
+		</div>
+	</div>
+	</td>
+	<td width="230px">
+		
+	</td>
+</tr>
+<tr height="120px">
+	<td width="283px">
+	  
+	</td>
+	<td>
+		
+	</td>
+	<td>
+		
+	</td>
+</tr>
+
+
+
+</body>
+</html>
